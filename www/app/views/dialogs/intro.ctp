f3d37ce15ced9f9d96c0967641252b69f8b25a9a
= <evolutioneer@gmail.com>
2011-07-11 01:19:54 -0400

    Tweaked the CSS on the dialog screen; added some variety in the colors of the response buttons.  Implemented the letter by letter add effect in the dialog box.

--- www/app/views/dialogs/intro.ctp
+++ www/app/views/dialogs/intro.ctp
@@ -1,2 +1,57 @@
-<h2>INTRODUCTION</h2>
-<p>Wait, please, no!  Don't hang up! ... </p>
+<div class="slide main-1">
+	<div class="face" value="robot"></div>
+	<div class="name" value="Technician Imp"></div>
+	<div class="next-button-target" value="main-2"></div>
+	<div class="append text">... Wait a moment... We've established a connection!  Whoever you are, please, don't hang up!</div>
+	<script type="text/javascript">
+		var SlideUpdaters = {};
+		
+		SlideUpdaters['main-1'] = function(node)
+		{
+			//$$debug is we being called??
+			console.log('***** Slide updater for main 1 called.');
+			//$$todo update the state of the menu button to show the help screen
+			//$$todo flash the "press here to advance" bubble over the next button
+		}; 
+	</script>
+</div>
+<div class="slide main-2">
+	<div class="next-button-target" value="main-3"></div>
+	<div class="append text">There isn't much time.  We're low on supplies and we need volunteers on the other side.  I want to put you through to our commander; he can explain the situation.</div>
+</div>
+<div class="slide main-3">
+	<div class="append text">Will you help us?</div>
+	<div class="append">
+		<div class="green dialog button" value="yes-1">YES</div>
+		<div class="red dialog button" value="no-1">NO</div>
+	</div>
+	<div class="append"><span class="instructions">Make your choice and press a button to continue.</span></div>
+</div>
+<div class="slide no-1">
+	<div class="face" value="robot_panic"></div>
+	<div class="append text">Our supplies are running low!  Please, don't hang up ... uh oh, the signal is -- </div>
+	<div class="append">
+		<div class="yellow dialog button" value="no-2">JUST KIDDING</div>
+	</div>
+</div>
+<div class="slide no-2">
+	<div class="next-button-target" value="main-4"></div>
+	<div class="face" value="robot_annoyed"></div>
+	<div class="append text">Thanks for the ulcer, Mac.  I mean, thanks for volunteering.  I'll patch you through to the commander.</div>
+</div>
+<div class="slide yes-1">
+	<div class="next-button-target" value="yes-2"></div>
+	<div class="face" value="robot_happy"></div>
+	<div class="append text">Thank goodness!  A sucker really IS born every minute!  I mean, you are a kind and generous soul.</div>
+</div>
+<div class="slide yes-2">
+	<div class="next-button-target" value="main-4"></div>
+	<div class="face" value="robot"></div>
+	<div class="append text">I'll patch you through to the commander now.  Hold please!</div>
+</div>
+<div class="slide main-4">
+	<div class="next-button-target" value="main-5"></div>
+	<div class="face" value="irmo"></div>
+	<div class="name" value="Commander Irmo"></div>
+	<div class="append text">I am Commander McFunkyPants.  Prepare to party hardy.  [END OF DEMO; INSERT 2 COINS]</div>
+</div>
--- www/app/webroot/js/dialog.js
+++ www/app/webroot/js/dialog.js
@@ -1,12 +1,164 @@
 var myScroll;
 
-$(document).ready(function() {
-	myScroll = new iScroll('dialog-wrapper');
-	$(window).bind('reorient', function() {
-		setTimeout(function() {
-			window.scrollTo(0, 1);
-			myScroll.refresh();
-		}, 100);
-	});
-	$.reorient.start();
-});
+var DialogScreen = (function() {
+	
+	var $data;
+	var $dialogBox;
+	var $nextButton;
+	var $name;
+	var $face;
+	var nextButtonTargetId;
+	var skippableTextBuild = false;
+	var skipTextBuild = false;
+	var textBuildInterval;
+	
+	// ------------------------------------------------------------------------
+	var init = function()
+	{
+		$data = $('.data');
+		$dialogBox = $('#dialog-wrapper .scroller');
+		$nextButton = $('.next.button');
+		$name = $('.sprite.name span');
+		$face = $('.sprite.face');
+		
+		myScroll = new iScroll('dialog-wrapper');
+		$(window).bind('reorient', function() {
+			setTimeout(function() {
+				window.scrollTo(0, 1);
+				myScroll.refresh();
+			}, 100);
+		});
+		$.reorient.start();
+		
+		//$$todo bind a screen change handler to all screen change buttons
+		$('.dialog.button').live('click', function(evt) {
+			changeSlide($(evt.target).attr('value'));
+		});
+		
+		changeSlide('main-1');
+	};
+	
+	// ------------------------------------------------------------------------
+	var changeSlide = function(newSlideId)
+	{
+		console.log('changeSlide(' + newSlideId + ')');
+		
+		//$$testme retrieve the data node for the new slide
+		var $slide = $('.slide.' + newSlideId);
+		
+		//$$debug what have we here?
+		console.log('$slide: ' + $slide);
+		
+		if(!$slide.length)
+		{
+			console.log('!!! No slide found by id ' + newSlideId + '. Aborting.');
+			return;
+		}
+		
+		//$$testme clear the contents of the scroller
+		$dialogBox.children().remove();
+		
+		skippableTextBuild = false;
+		
+		var $faceData = $('.face', $slide);
+		if($faceData) updateFace($faceData.attr('value'));
+		
+		var $nameData = $('.name', $slide);
+		if($nameData) updateName($nameData.attr('value'));
+		
+		var $nextButtonTarget = $('.next-button-target', $slide);
+		if($nextButtonTarget.length) nextButtonTargetId = $nextButtonTarget.attr('value');
+		else nextButtonTargetId = null;
+		
+		if(SlideUpdaters[newSlideId]) SlideUpdaters[newSlideId]($slide);
+		
+		//$$todo begin appending content in order
+		var $toAppend = $('.append', $slide);
+		
+		$toAppend.each(function(i, elem) {
+			//$$debug does this each work the way I think it does?
+			console.log('... i, elem: ' + i + ', ' + elem);
+			
+			if($(elem).hasClass('text'))
+			{
+				skippableTextBuild = true;
+				updateNextButton();
+				appendText($('<div />').appendTo($dialogBox), $(elem).text());
+			}
+			
+			else $dialogBox.append($(elem).children());
+		});
+	};
+	
+	// ------------------------------------------------------------------------
+	var appendText = function($appendTarget, text)
+	{
+		var $container = $appendTarget.append($('<div class="text-build" />'));
+		var nibble;
+		textBuildInterval = setInterval(function() {
+			if(skipTextBuild || text == '')
+			{
+				$container.append(text);
+				clearInterval(textBuildInterval);
+				skippableTextBuild = false;
+				skipTextBuild = false;
+				updateNextButton();
+			}
+			
+			else
+			{
+				nibble = text.substring(0, 1);
+				text = text.substring(1);
+				$container.append(nibble);
+			}
+			
+		}, 50);
+	};
+	
+	// ------------------------------------------------------------------------
+	var updateName = function(name)
+	{
+		$name.text(name);
+	}
+	
+	// ------------------------------------------------------------------------
+	var updateFace = function(faceId)
+	{
+		console.log('[NOT IMPLEMENTED] updateFace(' + faceId + ')');
+	};
+	
+	// ------------------------------------------------------------------------
+	var updateNextButton = function()
+	{
+		$nextButton.unbind('click');
+		
+		if(skippableTextBuild)
+		{
+			//console.log('[UPDATE NEXT BUTTON] Skippable text build!');
+			$nextButton
+				.removeClass('disabled')
+				.bind('click', function(evt) { skipTextBuild = true; });
+		}
+		
+		else if(nextButtonTargetId)
+		{
+			//console.log('[UPDATE NEXT BUTTON] Click to advance!');
+			$nextButton
+				.removeClass('disabled')
+				.bind('click', function(evt) { changeSlide(nextButtonTargetId); });
+		}
+		
+		else
+		{
+			//console.log('[UPDATE NEXT BUTTON] Can\'t skip ahead, disabling now.');
+			$nextButton.addClass('disabled');
+		}
+	};
+	
+	// PUBLIC INTERFACE -------------------------------------------------------
+	return {
+		init: init
+	};
+})();
+
+$(document).ready(DialogScreen.init);
